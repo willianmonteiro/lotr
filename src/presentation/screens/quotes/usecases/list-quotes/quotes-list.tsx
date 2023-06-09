26ccd1cd3b59e0f9953e21cda511b5c1aefa1e3a
Willian Monteiro <contato@willianmonteiro.com.br>
2023-06-08 23:18:57 -0300

    feat: quote list

---
+++ src/presentation/screens/quotes/usecases/list-quotes/quotes-list.tsx
@@ -0,0 +1,36 @@
+import { TQuote, TQuoteFilter } from "../../../../../app/domain";
+import { Card } from "../../../../components/card/card";
+import { Fallback } from "../../../../components/error/error-fallback";
+import { SearchContainer } from "../../../../components/search/search-container";
+import { useQuotesList } from "../../services/use-quotes-list";
+
+export function QuoteList({ by, filterBy }: TQuoteListProps) {
+  return (
+    <Fallback>
+      <SearchContainer
+        search={{
+          service: useQuotesList,
+          property:'dialog',
+          params: {
+            filterBy
+          }
+        }}
+        itemDisplayMapper={(filtered: TQuote[]) => filtered.map(({ _id, dialog }: TQuote) => (
+          <Card key={_id}>
+            <blockquote className="text-xl italic font-semibold text-gray-900">
+              <p>"{dialog}"</p>
+              {!!by && (
+                <small>- {by}</small>
+              )}
+            </blockquote>
+          </Card>
+        ))}
+      />
+    </Fallback>
+  )
+}
+
+type TQuoteListProps = {
+  by?: string
+  filterBy?: TQuoteFilter
+}
