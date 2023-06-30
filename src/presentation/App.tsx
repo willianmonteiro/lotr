2d207c5604df413f0af2fab5c402e141da95cbee
Willian Monteiro <willianmntro@gmail.com>
2023-06-30 16:05:34 -0300

    Update App.tsx

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <BrowserRouter basename='/willian_Project'>
+    <BrowserRouter basename='/lotr'>
       <AppProviders>
         <Header />
 
